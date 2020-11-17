413b09a0701cf38e7fd1b69337412c0e3c994043
Muthukumaran <muthuishere@gmail.com>
2020-11-17 17:13:15 +0530

    added docs

---
+++ publish-package.bat
@@ -0,0 +1,7 @@
+call ng build angular-sunburst-radar-chart --prod
+cd dist/angular-sunburst-radar-chart
+call npm publish
+cd ../../
+REM git add .
+REM git commit -m "Publishing new version"
+REM git push -u origin main
